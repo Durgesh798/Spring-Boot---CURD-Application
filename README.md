b51c8490806f26f65fc4ecd8abc50c6b4ec6fbf6
Durgesh798 <durgeshwarreddy.perolla@gmail.com>
2023-12-08 21:44:17 -0600

    updated model class

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Spring-Boot---CURD-Application
 
-Creating API and testing  using Postman
+Creating API and testing using Postman
 
 Database: MySQL 
+
+Clone Repo: git clone https://github.com/Durgesh798/Spring-Boot---CURD-Application.git
--- src/main/java/com/springboot/db/model/Student.java
+++ src/main/java/com/springboot/db/model/Student.java
@@ -6,9 +6,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name="students")
 public class Student {
 	
@@ -17,24 +23,5 @@ public class Student {
 	private long id;
 	private String name;
 	private String email;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
-	
 }
